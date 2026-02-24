98e2f4bf13049851d16a84b9ac270a157805ef6e
Bemnet Agegnehu <bemnetagegnehu859@gmail.comcd>
2026-02-24 20:36:44 +0300

    #

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { OrderProvider } from './context/OrderContext'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <OrderProvider>
         <App />
       </OrderProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
